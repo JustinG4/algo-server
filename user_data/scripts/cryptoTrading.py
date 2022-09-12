6168a89dbe49a5c01970c6d46803d98a82c6609f
JRG <JustinG4@users.noreply.github.com>
2022-09-12 15:13:25 -0400

    launch script update

--- user_data/scripts/cryptoTrading.py
+++ user_data/scripts/cryptoTrading.py
@@ -1,14 +1,14 @@
 import appscript
 import time
 
-def startCryptoBot():
-    script_1 = "docker start freqtrade"
-    appscript.app('Terminal').do_script(script_1)  # or any other command you choose
+def createSession():
+    script_0 = "ssh root @170.187.143.231 && docker-compose up -d"
+    appscript.app('Terminal').do_script(script_0)  # or any other command you choose
 
 def startGUI():
-    script_2 = "open -n -a 'Google Chrome' --args --new-window 'http://127.0.0.1:8080/trade'"
+    script_2 = "open -n -a 'Google Chrome' --args --new-window 'http://170.187.143.231:8080/trade'"
     appscript.app('Terminal').do_script(script_2)  # or any other command you choose
 
-startCryptoBot()
+createSession()
 time.sleep(5)
 startGUI()
